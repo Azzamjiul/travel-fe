658e579ca61145edef0185615f46beb509fc78b9
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2022-03-03 21:47:01 +0700

    Header Component

--- src/parts/iconText.js
+++ src/parts/iconText.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from 'elements/Button';
 
-export default function iconText() {
+export default function IconText() {
   return (
     <Button
         className='brand-text-icon'
